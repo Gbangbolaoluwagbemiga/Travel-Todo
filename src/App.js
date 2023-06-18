966947203e57b6db2fd1d44f78841602516e2742
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-06-18 22:23:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import {useState} from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <Stats items={[]} />
+    </div>
+  );
+}
+function Logo() {
+  return <h1 className="fw-bold">🏝️ Far Away 🧳</h1>;
+}
+
+function Form({onAddItems}) {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {description, quantity, packed: false, id: Date.now()};
+
+    onAddItems(newItem);
+
+    setDescription('');
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={e => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({length: 20}, (_, i) => i + 1).map(num => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function Stats({items}) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? 'You got everything! Ready to go ✈️'
+          : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
+
+export default App;
